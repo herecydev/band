725198564b8efa287c9abe5bda4c48f5d9758f55
herecydev <herecy@live.co.uk>
2023-12-03 08:47:33 +0000

    Add further bold for mobile

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,7 +8,7 @@ export default () => (
     <div className="h-screen bg-cover bg-[url('~/images/hero.jpg')]">
       <div className="flex h-screen">
         <section className="text-yellow-300 [text-shadow:_2px_2px_3px_black] m-auto">
-          <h1 className="text-3xl md:text-6xl text-center uppercase tracking-widest font-semibold">
+          <h1 className="text-3xl md:text-6xl text-center uppercase tracking-widest font-extrabold md:font-semibold">
             Kirky and The Sunflowers
           </h1>
           <nav className="mt-7 text-2xl">
