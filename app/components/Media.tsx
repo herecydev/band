ade74ee573dd33d8e6e5de6a2a02f5ea641eb8f5
Dan Kirkham <herecy@live.co.uk>
2024-02-10 15:27:40 +0000

    Max width

--- app/components/Media.tsx
+++ app/components/Media.tsx
@@ -5,7 +5,7 @@ import Youtube from "~/icons/youtube";
 export const Media = () => (
   <section id="media" className="my-10 md:my-20">
     <iframe
-      className="aspect-video w-5/6 md:w-3/4 mx-auto"
+      className="aspect-video w-5/6 md:w-3/4 max-w-5xl mx-auto"
       src="https://www.youtube-nocookie.com/embed/CEEdH4ethIE?si=vjyEsNdqRm3XjHrQ?autoplay=0&showinfo=0&controls=0"
       title="Kirky and The Sunflowers - Waiting"
       allowFullScreen
